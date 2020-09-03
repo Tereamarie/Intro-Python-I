103e558483c91301fa49156053748f3f4af165c5
marie-hotchkiss <mtknowlton67@gmail.com>
2020-09-03 10:12:02 -0700

    arg moduels completes all complied

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -7,17 +7,17 @@ changes, you'll get a TypeError saying you can't perform an operation
 on a string and an integer.
 """
 
-x = 5
-y = "7"
+#x = 5
+#y = "7"
 
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
 
-print(x + int(y))
+#print(x + int(y))
 
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
-print(str(x) + y)
+#print(str(x) + y)
